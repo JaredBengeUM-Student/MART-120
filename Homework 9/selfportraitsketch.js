26918d6dc7482f3afb170e3c7af36e5e450aeaf0
Jared McCann Benge <77377599+JaredBengeUM-Student@users.noreply.github.com>
2021-03-20 15:01:27 -0600

    Week 9

---
+++ Homework 9/selfportraitsketch.js
@@ -0,0 +1,48 @@
+function setup() 
+    {
+        createCanvas (600,600);
+    }
+    function draw() 
+    {
+        background(75,200,100);
+        textSize(25);
+        fill(51);
+        text("The COVID-19 NFT", 200, 50);
+
+        fill(0, 150, 255);
+        circle(300,125,125);
+
+        strokeWeight(5);
+        fill(0);
+        point(275,110);
+        point(325,110);
+
+        point (300,130);
+
+        ellipse (300, 155, 10, 15);
+
+        line(300,65,250,80);
+        line(300,65,350,80);
+
+        fill(150, 15, 100,);
+        rect(260,190,80,150);
+
+        fill(255);
+        triangle(300,155,330,140,340,130)
+
+        fill(10, 24, 120);
+        rect(340,190,25,115);
+
+        rect(235,190,25,115);
+
+        rect(260,340,25,100);
+
+        rect(315,340,25,100);
+
+        fill(51);
+        textSize(22);
+        text("- Jared Benge",225,500)
+    }
+
+
+  
